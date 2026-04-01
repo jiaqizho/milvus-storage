5ff319a8561db2abf84d1ec02077f22225683ad9
jiaqizho <jiaqi.zhou@zilliz.com>
2026-04-01 14:49:13 +0800

    feat: add Vortex V2 format reader and writer with row-group layout support

---
+++ cpp/include/milvus-storage/format/vortex/vortex_v2_writer.h
@@ -0,0 +1,19 @@
+// Copyright 2023 Zilliz
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This header is kept for backward compatibility.
+// VortexV2FileWriter has been merged into VortexFileWriter in vortex_writer.h.
+// Use VortexFileWriter with format_version=2 instead.
+#pragma once
+#include "milvus-storage/format/vortex/vortex_writer.h"
